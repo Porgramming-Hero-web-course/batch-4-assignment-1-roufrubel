b92fae71dda09fa7efa60658daefc2cf71235912
roufrubel <rubel.ma2012@gmail.com>
2024-11-09 11:47:30 +0000

    Problem-08 is done

--- Problem-05/Problem-05.ts
+++ Problem-05/Problem-05.ts
@@ -1,11 +1,11 @@
 // Problem-05
 
 
-const person = { name: "Alice", age: 30 }; 
+const personDetails = { name: "Alice", age: 30 }; 
 
 function getProperty<X, Y extends keyof X>(obj: X, key: Y): X[Y] {
     return obj[key];
 }
 
-console.log(getProperty(person, "name")); 
-console.log(getProperty(person, "age")); 
+console.log(getProperty(personDetails, "name")); 
+console.log(getProperty(personDetails, "age")); 
---
+++ Problem-08/Problem-08.ts
@@ -0,0 +1,12 @@
+// Problem-08
+
+
+const person = { name: "Rubel", age: 25, email: "rubel@gmail.com" };
+
+function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
+    return keys.every(key => key in obj);
+}
+
+
+console.log(validateKeys(person, ["name", "age"]));  
+console.log(validateKeys(person, ["name", "address"]));  
